6c48c7dc196a413a9a276d7adb65b1d977f31082
arson-dev <anasalabson@gmail.com>
2022-11-29 12:08:13 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      {/* Header */}
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
     </div>
   )
 }
